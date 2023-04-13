bc20c2b9acc07226d2748857ab21b6a8d890fe93
Elian Emir Hadad <hadad.elian@gmail.com>
2023-04-12 23:15:13 -0300

    se implementó con éxito get de profile

--- portfolio/src/main/java/com/portfolioehadad/portfolio/controller/UserController.java
+++ portfolio/src/main/java/com/portfolioehadad/portfolio/controller/UserController.java
@@ -3,6 +3,7 @@ package com.portfolioehadad.portfolio.controller;
 import com.portfolioehadad.portfolio.models.UserPorfolio;
 import com.portfolioehadad.portfolio.service.AuthService;
 import com.portfolioehadad.portfolio.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,5 +24,8 @@ public class UserController {
     public ResponseEntity<?> login(@RequestBody UserPorfolio user) throws Exception {
         return authService.login(user);
     }
-
+    @GetMapping(value = "/user")
+    public ResponseEntity<?> getUser(HttpServletRequest req) throws Exception {
+        return userService.getUserAndProfile(req);
+    }
 }
--- portfolio/src/main/java/com/portfolioehadad/portfolio/service/UserService.java
+++ portfolio/src/main/java/com/portfolioehadad/portfolio/service/UserService.java
@@ -1,9 +1,31 @@
 package com.portfolioehadad.portfolio.service;
 
+import com.portfolioehadad.portfolio.models.Profile;
+import com.portfolioehadad.portfolio.models.UserPorfolio;
+import com.portfolioehadad.portfolio.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class UserService  {
-
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    AuthService authService;
+    public ResponseEntity<?> getUserAndProfile(HttpServletRequest req) throws Exception{
+        try{
+            UserPorfolio user = authService.authChecker(req);
+            if (user == null){
+                return ResponseEntity.badRequest().body("No puede acceder a la información");
+            }
+            Profile profile = user.getProfile();
+            return new ResponseEntity<>(profile, HttpStatus.OK);
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
 }
